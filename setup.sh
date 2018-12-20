533b7f1baec0ea7f99ca4642ed204716e268ca86
Enda Phelan <ephelan@redhat.com>
2018-12-20 16:51:14 +0000

    Added auto symlinks to setup scripts

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
-ln -s
+#!/bin/bash
+ln -sf $PWD/.zshrc $HOME/.zshrc
+ln -sf $PWD/.aliases $HOME/.aliases
+ln -sf $PWD/.envs $HOME/.envs
+ln -sf $PWD/.gitconfig $HOME/.gitconfig
+
+if [ ! -f $PWD/.secrets ]; then
+    ln -sf $PWD/.secrets $HOME/.secrets
+fi
