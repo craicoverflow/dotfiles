380c1c898b82cb5c304cb6eb35bc72463793d844
Enda Phelan <ephelan@redhat.com>
2019-01-02 15:55:43 +0000

    Added new aliases

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ ln -sf $PWD/.aliases $HOME/.aliases
 ln -sf $PWD/.envs $HOME/.envs
 ln -sf $PWD/.gitconfig $HOME/.gitconfig
 
-if [ ! -f $PWD/.secrets ]; then
+if [ -f $PWD/.secrets ]; then
     ln -sf $PWD/.secrets $HOME/.secrets
 fi
