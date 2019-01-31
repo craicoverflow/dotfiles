2641d067fb9a42e2d87c82827306f6a33e05962e
Enda Phelan <ephelan@redhat.com>
2019-01-31 09:19:06 +0000

    Created a git message template

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+# Symlink dotfiles
 ln -sf $PWD/.zshrc $HOME/.zshrc
 ln -sf $PWD/.aliases $HOME/.aliases
 ln -sf $PWD/.envs $HOME/.envs
 ln -sf $PWD/.gitconfig $HOME/.gitconfig
+ln -sf $PWD/.gitmessage $HOME/.gitmessage
+
+# Symlink startup script
+ln -sf $PWD/init.d/startup.sh /etc/init.d/startup
 
 if [ -f $PWD/.secrets ]; then
     ln -sf $PWD/.secrets $HOME/.secrets
