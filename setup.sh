9d727291eac327c4e18c5c85125f01a4d8f5ec74
Enda Phelan <ephelan@redhat.com>
2019-03-28 10:54:26 +0000

    feat: Added tmux config

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ ln -sf $PWD/.aliases $HOME/.aliases
 ln -sf $PWD/.envs $HOME/.envs
 ln -sf $PWD/.gitconfig $HOME/.gitconfig
 ln -sf $PWD/.gitmessage $HOME/.gitmessage
+ln -sf $PWD/.tmux.conf $HOME/.tmux.conf
 
 # Symlink startup script
 ln -sf $PWD/init.d/startup.sh /etc/init.d/startup
