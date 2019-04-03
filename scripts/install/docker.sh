a33f09f1bda41a97689195e0dc3532d5dde1bf2a
Enda Phelan <ephelan@redhat.com>
2019-04-03 23:07:55 +0100

    added docker install script

--- scripts/install/docker.sh
+++ scripts/install/docker.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
-dnf install vim -y
+dnf -y install dnf-plugins-core
+
+# Get the current Fedora version
+releasever=$(sed -e 's#.*=\(\)#\1#' <<< "$(cat /etc/os-release | grep "VERSION_ID=*")")
+
+cat <<EOF | sudo tee /etc/yum.repos.d/docker-ce.repo
+[docker-ce-stable]
+name=Docker CE Stable
+baseurl=https://download.docker.com/linux/fedora/$releasever/x86_64/stable
+enabled=1
+gpgcheck=1
+gpgkey=https://download.docker.com/linux/fedora/gpg
+EOF
+
+dnf -y install docker-ce
+
+systemctl enable --now docker
+
+usermod -aG docker $(whoami)
