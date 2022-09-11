ea02c279cb671d6cafca248a5799a34cc31e5564
Enda Phelan <endaphelan@gmail.com>
2022-09-11 16:05:40 +0100

    add script to install configurations

---
+++ setup.sh
@@ -0,0 +1,37 @@
+# /bin/sh
+
+does_binary_exist() {
+  if ! command -v $1 &> /dev/null
+  then
+    echo "$1 could not be found"
+    return 1
+  fi
+  return 0
+}
+
+breww() {
+  if ! does_binary_exist "brew"
+  then
+    echo "brew is missing from your computer. Installing it for you..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+  if [ "$1" == "install" ]; then
+    echo "Installing package '$2'"
+  fi
+  brew "$@"
+}
+
+echo "Setting up Neovim..."
+breww install neovim
+mkdir -p $HOME/.config
+ln -sfn "$(pwd)/nvim" $HOME/.config
+
+echo "Setting up vim-plug..."
+# Install vim-plug package manager
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+vim +'PlugInstall --sync' +qa
+
+echo "Installing 'Inconsolata Mono Nerd Font'..."
+cd $HOME/Library/Fonts && curl -fLo "Inconsolata Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Inconsolata/complete/Inconsolata%20Regular%20Nerd%20Font%20Complete%20Mono.otf?raw=true
+
