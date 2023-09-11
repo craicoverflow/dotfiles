c679758f724fc3d930be1502f448716c93a0aac1
Enda Phelan <endaphelan@Endas-Air.station>
2023-09-11 20:09:41 +0100

    chore: add direnv to profile

--- install.sh
+++ install.sh
@@ -9,10 +9,6 @@ cd ~/.dotfiles
 
 echo "export DOTFILES_ROOT=$(pwd)" > ~/.dotfilesrc
 
-echo "" >> ~/.zshrc
-echo "## Temporarily adds dotfiles binary to path. Run 'dotfiles apply' to overwrite" >> ~/.zshrc
-echo "export PATH=$(pwd)/bin:$PATH" >> ~/.zshrc
-
 >&2 echo ""
 >&2 echo "dotfiles installed successfully. Run 'dotfiles install' to install packages"
 
