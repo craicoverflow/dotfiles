847b71000adf05d570aaa79c8184ac8b496c448b
Enda Phelan <endaphelan@gmail.com>
2023-09-17 12:45:59 +0100

    only show debug messages when DEBUG=true

--- cmd/_util.sh
+++ cmd/_util.sh
@@ -2,13 +2,16 @@
 
 OS="$(uname -s | tr '[:upper:]' '[:lower:]')"
 ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')"
+DEBUG="${DEBUG:-false}"
 
 log::info() {
   >&2 echo $1
 }
 
 log::debug() {
-  >&2 echo "[DEBUG] $1"
+  if [ "$DEBUG" == true ]; then
+    log::info "[$(date +%F_%T)] [DEBUG] $1"
+  fi
 }
 
 fail() {
