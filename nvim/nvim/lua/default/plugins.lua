b3b2d0debd7c0937533e9a73dadbbe8f877b85f4
Enda Phelan <penda@vmware.com>
2024-04-16 15:38:48 +0100

    push instead of pull dotfiles

---
+++ nvim/nvim/lua/default/plugins.lua
@@ -0,0 +1,68 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.6',
+		-- or                            , branch = '0.1.x',
+		requires = { 
+      {'nvim-lua/plenary.nvim'},
+      { "nvim-telescope/telescope-live-grep-args.nvim" }
+    },
+    config = function ()
+      require("telescope").load_extension("live_grep_args")
+    end
+	} 
+
+	use({
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+		end
+	})
+
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use {
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		requires = { {"nvim-lua/plenary.nvim"} }
+	}
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+
+  --  use({
+  --  'linrongbin16/gitlinker.nvim',
+  --  config = function()
+  --    require('gitlinker').setup()
+  --  end
+  --})
+
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			--- Uncomment the two plugins below if you want to manage the language servers from neovim
+			-- {'williamboman/mason.nvim'},
+			-- {'williamboman/mason-lspconfig.nvim'},
+
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip'},
+		}
+	}
+
+	use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+		require("toggleterm").setup()
+	end}
+
+  use('tpope/vim-rhubarb')
+end)
